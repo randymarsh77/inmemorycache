619161118b55ac2230ed0492359047d3fecbebcd
randymarsh77 <mattpsbs@gmail.com>
2021-01-31 09:28:14 -0800

    Update to Swift 5.1.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,21 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "InMemoryCache",
-    dependencies: [
-		.Package(url: "https://github.com/randymarsh77/pubsubcache", majorVersion: 0),
+	name: "InMemoryCache",
+	products: [
+		.library(
+			name: "InMemoryCache",
+			targets: ["InMemoryCache"]
+		),
+	],
+	dependencies: [
+		.package(url: "https://github.com/randymarsh77/pubsubcache", .branch("master")),
+	],
+	targets: [
+		.target(
+			name: "InMemoryCache",
+			dependencies: ["PubSubCache"]
+		),
 	]
 )
